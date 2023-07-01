3bda1ca8ab668f6c28d967465e23eb01732a7c4b
lombrana <mlombraa@gmail.com>
2023-07-01 11:03:40 +0200

    1º commit

---
+++ main.js
@@ -0,0 +1,109 @@
+const products = [
+    {
+    name: 'HP Essentials 255 G8 AMD',
+    price: 289,
+    stars: 4,
+    reviews: 250,
+    Proyecto DOM 2
+    seller: 'PcComponentes',
+    image: 'https://cdn.grupoelcorteingles.es/SGFM/dctm/MEDIA03/202003/19/00119810401521____11__640x640.jpg'
+    
+    },
+
+    {
+    name: '2',
+    price: 289,
+    stars: 4,
+    reviews: 250,
+    seller: 'PcComponentes',
+    image: 'https://cdn.grupoelcorteingles.es/SGFM/dctm/MEDIA03/202102/15/00119810401638____12__640x640.jpg
+    },
+
+    {
+    name: '3',
+    price: 289,
+    stars: 4,
+    reviews: 250,
+    seller: 'PcComponentes',
+    image: 'https://cdn.grupoelcorteingles.es/SGFM/dctm/MEDIA03/202203/02/00119810401745____19__640x640.jpg'    },
+
+    {
+    name: '4',
+    price: 289,
+    stars: 4,
+    reviews: 250,
+    Proyecto DOM 2
+    seller: 'PcComponentes',
+    image: 'https://thumb.pccomponentes.com/w-300-300/articles/1005/10057282/1639-hp-essential-255-g8-amd-3020e-8gb-256gb-ssd-156.jpg'
+    },
+
+    {
+    name: '5',
+    price: 289,
+    stars: 4,
+    reviews: 250,
+    Proyecto DOM 2
+    seller: 'PcComponentes',
+    image: 'https://thumb.pccomponentes.com/w-300-300/articles/1005/10057282/1639-hp-essential-255-g8-amd-3020e-8gb-256gb-ssd-156.jpg'
+    },
+    
+    {
+    name: '6',
+    price: 289,
+    stars: 4,
+    reviews: 250,
+    Proyecto DOM 2
+    seller: 'PcComponentes',
+    image: 'https://thumb.pccomponentes.com/w-300-300/articles/1005/10057282/1639-hp-essential-255-g8-amd-3020e-8gb-256gb-ssd-156.jpg'
+    },
+
+    {
+    name: '7',
+    price: 289,
+    stars: 4,
+    reviews: 250,
+    Proyecto DOM 2
+    seller: 'PcComponentes',
+    image: 'https://thumb.pccomponentes.com/w-300-300/articles/1005/10057282/1639-hp-essential-255-g8-amd-3020e-8gb-256gb-ssd-156.jpg'
+    },
+
+    {
+    name: '8',
+    price: 289,
+    stars: 4,
+    reviews: 250,
+    Proyecto DOM 2
+    seller: 'PcComponentes',
+    image: 'https://thumb.pccomponentes.com/w-300-300/articles/1005/10057282/1639-hp-essential-255-g8-amd-3020e-8gb-256gb-ssd-156.jpg'
+    },
+
+    {
+    name: '9',
+    price: 289,
+    stars: 4,
+    reviews: 250,
+    Proyecto DOM 2
+    seller: 'PcComponentes',
+    image: 'https://thumb.pccomponentes.com/w-300-300/articles/1005/10057282/1639-hp-essential-255-g8-amd-3020e-8gb-256gb-ssd-156.jpg'
+    },
+
+    {
+    name: '10',
+    price: 289,
+    stars: 4,
+    reviews: 250,
+    Proyecto DOM 2
+    seller: 'PcComponentes',
+    image: 'https://thumb.pccomponentes.com/w-300-300/articles/1005/10057282/1639-hp-essential-255-g8-amd-3020e-8gb-256gb-ssd-156.jpg'
+    },
+
+
+
+
+
+
+
+
+
+
+]
